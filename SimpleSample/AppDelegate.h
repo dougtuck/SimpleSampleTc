7d6918da0d53ea663ef1aef71b9779a227c7b463
doug <doug@tuckerconsulting.com>
2012-02-08 14:44:03 -0800

    Initial Commit

---
+++ SimpleSample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SimpleSample
+//
+//  Created by Douglas Tucker on 2/8/12.
+//  Copyright (c) 2012 Tucker Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SimpleSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SimpleSample
+//
+//  Created by Douglas Tucker on 2/8/12.
+//  Copyright (c) 2012 Tucker Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
