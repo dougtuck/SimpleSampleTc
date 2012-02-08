221ab49a31e30fbcdeaca4a082d00abedb3cf002
doug <doug@tuckerconsulting.com>
2012-02-08 15:45:30 -0800

    Added UI support objects and functions

--- SimpleSample/ViewController.m
+++ SimpleSample/ViewController.m
@@ -7,8 +7,62 @@
 //
 
 #import "ViewController.h"
+#import "AboutViewController.h"
 
 @implementation ViewController
+
+
+/***************************************************************
+ *  UI Functions
+ **************************************************************/
+- (IBAction)changeButton:(id)sender {
+    // Toggle the button state
+    bGreenButton = !bGreenButton;
+    
+    if (bGreenButton) {
+        // Display the Green speaker
+        [speakerButton setImage:[UIImage imageNamed:@"AudioOn1.png"] forState:UIControlStateNormal];
+        [outputLabel setText:@"Green Speaker"];
+    } else {
+        // Display the Red speaker
+        [speakerButton setImage:[UIImage imageNamed:@"AudioOff2.png"] forState:UIControlStateNormal];
+        [outputLabel setText:@"Red Speaker"];
+    }
+}
+
+- (IBAction)showAbout:(id)sender {
+    AboutViewController *as = [[AboutViewController alloc] init];
+    
+    // Push it onto the Nav controllers stack
+    [[self navigationController] pushViewController:as animated:true];
+}
+
+/***************************************************************
+ *  Init Functions
+ **************************************************************/
+- (id)init { 
+    
+    // Call the superclass's designated initializer 
+    self = [super init]; 
+    if (self) {
+        bGreenButton = false;
+    }
+    return self;
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+        // Set the title of the navigation item 
+        [[self navigationItem] setTitle:@"Simple Sample"]; 
+        
+        UIBarButtonItem *leftButton = [[UIBarButtonItem alloc] initWithTitle:@"About" style:UIBarButtonItemStylePlain target:self action:@selector(showAbout:)];
+        self.navigationItem.leftBarButtonItem = leftButton;
+    }
+    return self;
+}
 
 - (void)didReceiveMemoryWarning
 {
